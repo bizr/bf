09cde09684c0d52a33b37574290101506863eb37
pip4so <souliotis@bizr.gr>
2025-08-06 20:07:26 +0300

    Initial commit - Mediterranean breakfast delivery app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { ShoppingCart, Clock, Plus, Minus, ArrowLeft, Check, MapPin } from 'lucide-react';
+
+const BreakfastShopApp = () => {
+  // Paste your entire component code here
+  // (all the code from the artifact)
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <BreakfastShopApp />;
 }
 
 export default App;
